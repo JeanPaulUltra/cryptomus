8f1d8f4abd9693c79cc319647a41419e17baf153
mehedishaon29@gmail.com <mehedishaon29@gmail.com>
2024-03-25 22:43:00 +0600

    d

--- src/Routes/api.php
+++ src/Routes/api.php
@@ -1,5 +1,5 @@
 <?php
 
 Route::group(['prefix' => 'api',  'middleware' => 'api'], function() {
-    Route::post('cryptomus/webhook', '\Kristof\Cryptomus\Http\Controllers\WebhookController')->name('cryptomus-webhook');
+    Route::post('cryptomus/webhook', '\Kristof202\Cryptomus\Http\Controllers\WebhookController')->name('cryptomus-webhook');
 });
